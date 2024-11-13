2d529d64523047756d879d94d573b77e9d26eddb
Leonardo Suárez <leonardojuliansuarezamezquita@MacBook-Pro-de-Leonardo.local>
2024-11-13 18:48:29 -0300

    Agregamos botones para compartir

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,13 +1,32 @@
-import { Box, Grid2, ImageList, ImageListItem, Paper, Typography } from '@mui/material'
+import { Box, ButtonGroup, Grid2, ImageList, ImageListItem, Paper, Typography, Button } from '@mui/material'
 import { ProductProps } from '../types'
 import { FC } from 'react'
-
+import { Share } from '@mui/icons-material'
 
 const Product:FC<ProductProps> = ({ product }) => {
+  const generateSlug = (text: string): string =>{
+    return text
+        .toLowerCase()
+        .trim()
+        .replace(/[^a-z0-9\s-]/g, '') // Elimina caracteres no alfanuméricos
+        .replace(/\s+/g, '-')         // Reemplaza espacios por guiones
+        .replace(/-+/g, '-');         // Reemplaza múltiples guiones por uno solo
+  }
+
+  const productKey = generateSlug(product.name)
+
+  const infoRequest = () => { 
+    open(`https://api.whatsapp.com/send?phone=+5491130361238&text=Hola!, quiero tener mas información acerca de ${product.name} que tienes en venta.\n ${window.location.origin}/#${productKey}`, '_blank') 
+  }
+
+  const shareAction = () => {
+    open(`https://api.whatsapp.com/send?text=Hola, Te comparto este producto que te puede interesar ${window.location.origin}/#${productKey}`, '_blank')
+  }
+
   return (
     <>
-     <Box sx={{ width: '100%'}}>
-      <Paper sx={{ overflow: 'hidden', mb: 4, p:2 }} elevation={2}>
+     <Box sx={{ width: '100%'}} id={ productKey }>
+      <Paper sx={{ overflow: 'hidden', mb: 4, p:2, borderRadius: 1 }} elevation={3}>
         <Grid2 container spacing={2}>
           <Grid2 size={{ xs:12, md:7 }} sx={{ overflow: 'hidden', overflowY: 'hidden', overflowX: 'scroll' }}>
             <ImageList sx={{ maxWidth: '100%'  }} cols={ product.pictures.length > 1 ? 2 : 1}>
@@ -30,7 +49,16 @@ const Product:FC<ProductProps> = ({ product }) => {
                 sx={{ color: '#2d3436', textAlign: 'left' }}
                 dangerouslySetInnerHTML={{ __html: product.description }}></Typography>
               <p>{ product.price }</p>
-              <button>Comprar</button>
+
+              <ButtonGroup size="large">
+                <Button onClick={ () => infoRequest() }>Consultar</Button>
+
+
+                <Button onClick={ () => shareAction() }>
+                  Compartir en Whatsapp <Share />
+                </Button>
+              </ButtonGroup>
+              
             </Box>
           </Grid2>
         </Grid2>
--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -2,8 +2,8 @@ export interface ProductList {
     products: (Product)[] | [];
 }
 export interface Product {
-name: string;
-description: string;
-price?: number | 0;
-pictures: (string)[] | [];
+    name: string;
+    description: string;
+    price?: number | 0;
+    pictures: (string)[] | [];
 }
