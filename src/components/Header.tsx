4e916dfe612bed38783639d6e564fba82ef38749
Leonardo Suárez <leonardojuliansuarezamezquita@MacBook-Pro-de-Leonardo.local>
2024-11-13 22:04:11 -0300

    Agregamos productos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from '@mui/material'
+import { Share } from '@mui/icons-material'
+import { Box, Button, Typography } from '@mui/material'
 
 export const Header = () => {
     const fontTheme = {
@@ -7,9 +8,13 @@ export const Header = () => {
         fontWeight: 'lighter'
     }
 
+    const shareSite = () => {
+        open(`https://api.whatsapp.com/send?text=Hola, Un amigo se esta mudando y esta vendiendo algunas cosas, te comparto la lista ${window.location.origin}`, '_blank')
+    }
+
     return (
         <>
-            <Box component={'header'} sx={{ with: '100%', p: 4 }}>
+            <Box component={'header'} sx={{ with: '100%', p: 4, textAlign: 'center' }}>
                 <Typography 
                     variant="h6"
                     component={'h1'}
@@ -17,6 +22,12 @@ export const Header = () => {
                 >
                     Hola!, soy Leo. Me estoy mudando y te comparto algunas cositas de casa que estan en venta.
                 </Typography>
+                <Button variant="contained" 
+                    onClick={ () => shareSite() } 
+                    sx={{ 
+                        alignContent: 'center',
+                        mt: 2
+                    }}>Compartir es sitio <Share /></Button>
             </Box>
         </>
     )
