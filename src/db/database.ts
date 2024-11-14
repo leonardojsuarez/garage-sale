697f4639e429501e650427c6f6d18760c6283e73
Leonardo Suárez <leonardojuliansuarezamezquita@MacBook-Pro-de-Leonardo.local>
2024-11-13 21:28:04 -0300

    Agregamos botones para compartir

--- src/db/database.ts
+++ src/db/database.ts
@@ -9,7 +9,7 @@ const data = {
       `,
       "price": 240000,
       "pictures": [
-        "images/products/colchón.webp"
+        "images/products/colchon1.webp"
       ]
     },
     {
@@ -17,10 +17,10 @@ const data = {
       "description": "<h3 dir='ltr'>Sill&oacute;n&nbsp;</h3><ul><li dir='ltr'><strong>Ancho:</strong> 180 cm</li><li dir='ltr'><strong>Profundidad:</strong> 85 cm</li><li dir='ltr'><strong>Alto:</strong> 85 cm<strong><br></strong></li></ul><h3 dir='ltr'>Camastro</h3><ul><li dir='ltr'><strong>Ancho:</strong> 54 cm&nbsp;</li><li dir='ltr'><strong>Alto:</strong> 44 cm</li></ul>",
       "price": 322000,
       "pictures": [
-        "images/products/IMG_4188.jpg",
-        "images/products/IMG_4203.jpg",
-        "images/products/IMG_4204.jpg",
-        "images/products/IMG_4206.jpg"
+        "images/products/sillon_1.jpg",
+        "images/products/sillon_2.jpg",
+        "images/products/sillon_3.jpg",
+        "images/products/sillon_4.jpg"
       ]
     },
     {
@@ -28,7 +28,19 @@ const data = {
       "description": "<ul><li>Rodado 20</li><li>Cuadro de aluminio</li><li>7 velocidades</li></ul>",
       "price": 660000,
       "pictures": [
-        "images/products/IMG_9537.jpg"
+        "images/products/bici_1.jpg"
+      ]
+    },
+    {
+      "name": "Cama + 2 mesas de luz minimalista",
+      "description": `
+        <p dir="ltr"><strong>Largo: </strong>195 cm</p>
+        <p dir="ltr"><strong>Ancho:</strong> 161 cm</p>
+        <p><strong>Alto:</strong> 30 cm</p>
+      `,
+      "price": 150000,
+      "pictures": [
+        "images/products/cama_1.jpg"
       ]
     }
   ]
